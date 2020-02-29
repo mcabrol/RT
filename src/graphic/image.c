17f9d784383db0f1e69d9ad12d733bdeb7b9d1bc
mcabrol <martin.cabrol@free.fr>
2020-02-29 14:44:35 +0100

    Update src/graphic/image.c

--- src/graphic/image.c
+++ src/graphic/image.c
@@ -40,16 +40,16 @@ void 	write_ppm(t_rtv1 *rtv1)
 	time_t	stamp;
 	int 	i;
 	int 	fd;
-	t_vec	*screen;
+	t_scene	*scene;
 
-	screen = rtv1->render.screen;
+	scene = &rtv1->scene;
 	stamp = time(NULL);
 	i = -1;
 	fd = open(ft_strcat(ctime(&stamp), ".ppm"), O_WRONLY | O_APPEND | O_CREAT, 0644);
-	ft_dprintf(fd, "P3\n%d %d\n%d\n", rtv1->scene.width, rtv1->scene.height, 255);
-	while (++i < rtv1->scene.width * rtv1->scene.height)
-		ft_dprintf(fd, "%d %d %d ", to_byte(screen[i].x, GAMMA),
-			                     	to_byte(screen[i].y, GAMMA),
-			                     	to_byte(screen[i].z, GAMMA));
+	ft_dprintf(fd, "P3\n%d %d\n%d\n", scene->width, scene->height, 255);
+	while (++i < scene->width * scene->height)
+		ft_dprintf(fd, "%d %d %d ", to_byte(rtv1->render.screen[i].x, GAMMA),
+			                     	to_byte(rtv1->render.screen[i].y, GAMMA),
+			                     	to_byte(rtv1->render.screen[i].z, GAMMA));
 	close(fd);
 }
